3c3ffa49c34b4ec19bb8e05680b7691b0fc84a88
tanvi1706 <tanvikrishnapatil@tanvis-MBP.lan>
2021-03-13 04:45:04 -0600

    final commit V 0.0.3

--- lib/main.dart
+++ lib/main.dart
@@ -10,14 +10,85 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blueAccent,
+        backgroundColor: Colors.white,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               CircleAvatar(
                 radius: 50.0,
                 backgroundImage: AssetImage('Images/IMG_20201210_153103.jpg'),
               ),
+              Text(
+                'Tanvi Krishna Patil',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.normal,
+                  color: Colors.black45,
+                ),
+              ),
+              Text(
+                'SOFTWARE DEVELOPER, DESIGNER',
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 15.0,
+                  letterSpacing: 2.0,
+                  fontWeight: FontWeight.normal,
+                  color: Colors.black38,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 200.0,
+                child: Divider(
+                  color: Colors.black,
+                ),
+              ),
+              Card(
+                color: Colors.pink[200],
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                child: Padding(
+                  padding: EdgeInsets.all(2.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.white,
+                      size: 30.0,
+                    ),
+                    title: Text(
+                      '+1 (765) 295 0643',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.pink[400],
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                child: Padding(
+                  padding: EdgeInsets.all(2.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.send,
+                      color: Colors.white,
+                      size: 30.0,
+                    ),
+                    title: Text(
+                      'tani.patil.1997@hotmail.com',
+                      style: TextStyle(
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              )
             ],
           ),
         ),
